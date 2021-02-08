01923d42066237103ad5824e2fc4d55dd34738d0
Junayed <muhammadjunayed2424@gmail.com>
2021-02-08 12:04:56 +0600

    added new

---
+++ Api.js
@@ -0,0 +1,54 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(res => res.json())
+    .then(data => {
+        displayFoods(data.categories);
+    })
+const displayFoods = foods => {
+    const displayFoodsDiv = document.getElementById('displayFoodsId');
+    foods.forEach(food => {
+        let displayFoodDiv = document.createElement('div');
+        displayFoodDiv.className = 'displayFood';
+        const displayFood =
+            `
+        <div style="cursor:pointer" onclick="displayFoodsDetail('${food.strCategory}')">
+        <img src ="${food.strCategoryThumb}">
+        <h3 class ="food-name">${food.strCategory}</h3>
+        <p>
+        </div>
+        `;
+        displayFoodDiv.innerHTML = displayFood;
+        displayFoodsDiv.appendChild(displayFoodDiv);
+    });
+}
+const displayFoodsDetail = name => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            detailFoodsInfo(data.meals);
+        })
+
+}
+const detailFoodsInfo = foods => {
+    const foodDiv = document.getElementById('foodDetails');
+    foods.forEach(food => {
+        foodDiv.innerHTML =
+            `
+            <img style="width:100%;border-radius:8px;margin:auto;" src="${food.strMealThumb}">
+            <h1 style="text-align:center;">${food.strCategory}</h1>
+            <p style="text-align:center;">${food.strMeal}</p>
+            <p style="text-align:center;">Area:${food.strArea}</p>
+            <h5 style="text-align:center;">Recipe:</h5>
+            <p style="text-align:center;">✅${food.strIngredient2}</p>
+            <p style="text-align:center;">✅${food.strIngredient3}</p>
+            <p style="text-align:center;">✅${food.strIngredient4}</p>
+            <p style="text-align:center;">✅${food.strIngredient5}</p>
+            <p style="text-align:center;">✅${food.strIngredient6}</p>
+            <p style="text-align:center;">✅${food.strIngredient7}</p>
+
+            `
+    });
+}
